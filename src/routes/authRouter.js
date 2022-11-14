e9ba9fbef25af66d691893eacaff834c8b0493bd
pablo-zalazar <pablo_zalazar@outlook.com>
2022-11-14 19:13:28 -0300

    auth endpoints v1

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { userRegister, userLogin } from '../controllers/authController.js';
+import {
+  userRegister,
+  userLogin,
+  userChangePassword,
+  userLogout,
+} from '../controllers/authController.js';
 import checkAuth from '../middlewares/checkAuth.js';
 
 const authRouter = express.Router();
 
 authRouter.route('/register').post(userRegister);
 authRouter.route('/login').post(userLogin);
+authRouter.route('/changePassword/:id').post(checkAuth, userChangePassword);
+authRouter.route('/logout').post(checkAuth, userLogout);
 
 export default authRouter;
