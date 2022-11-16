f2b3e731e6b901e1e0ed041c4f935d5a9995ff40
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-11-16 17:56:44 +0100

    corsFix

--- src/server/index.js
+++ src/server/index.js
@@ -8,11 +8,17 @@ import authRouter from "../routes/authRouter.js";
 const server = express();
 server.use(express.json());
 
-server.use(
-  cors({
-    origin: "http://localhost:5173"
-  })
-);
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (process.env.ORIGINS_ALLOWED.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+server.use(cors(corsOptionsDelegate));
 
 connectDB();
 
