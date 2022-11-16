a757171a289c1239279f5c920a86439da1492c02
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-11-16 15:29:43 +0100

    cors intento 3

--- src/server/index.js
+++ src/server/index.js
@@ -25,14 +25,9 @@ cloudinary.config({
 connectDB();
 
 server.use(helmet());
-server.use(
-  cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
-    credentials: true,
-    preflightContinue: true,
-    optionsSuccessStatus: 204
-  })
-);
+
+// CORS authorisation
+app.use(cors);
 
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
