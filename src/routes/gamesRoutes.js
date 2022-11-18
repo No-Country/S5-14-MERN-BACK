73be62db1701c7c03fa2cff8e3a01d0216fa8c45
requin883 <requin883@gmail.com>
2022-11-18 06:43:05 -0800

    add eliminate game function for the method delete

--- src/routes/gamesRoutes.js
+++ src/routes/gamesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createNewGame, findAllGames, findGameById, modifyExistingGame } from "../controllers/gamesController.js";
+import { createNewGame, eliminateGame, findAllGames, findGameById, modifyExistingGame } from "../controllers/gamesController.js";
 import checkAuth from "../middlewares/checkAuth.js";
 
 const gamesRouter = express.Router();
@@ -9,7 +9,7 @@ gamesRouter.route("/")
     .post(checkAuth, createNewGame);
 
 gamesRouter.route("/:id").get(findGameById)
-    .put(checkAuth, modifyExistingGame);
-
+    .put(checkAuth, modifyExistingGame)
+    .delete(checkAuth, eliminateGame);
 
 export default gamesRouter;
