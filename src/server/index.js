3a93f77c1c261d8f9d93da8c36774705cb4c811a
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-11-25 18:28:38 +0100

    cross ok

--- src/server/index.js
+++ src/server/index.js
@@ -8,34 +8,26 @@ import authRouter from "../routes/authRouter.js";
 const server = express();
 server.use(express.json());
 
-// var corsOptionsDelegate = {
-//   origins: function (req, callback) {
-//     var corsOptions;
-//     if (process.env.ORIGINS_ALLOWED.indexOf(req.header("Origin")) !== -1) {
-//       corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-//     } else {
-//       corsOptions = { origin: false }; // disable CORS for this request
-//     }
-//     callback(null, corsOptions); // callback expects two parameters: error and options
-//   },
-//   optionsSuccessStatus: 200,
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-// };
+// CORS authorisation
+var corsOptionsDelegate = {
+  origins: function (req, callback) {
+    var corsOptions;
+    if (process.env.ORIGINS_ALLOWED.indexOf(req.header("Origin")) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  },
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+};
 
-//server.use(cors({ origin: corsOptionsDelegate }));
-server.use(
-  cors({
-    origin: ["http://localhost:5173/", "http://localhost:5173", "http://127.0.0.1:5173"],
-    optionsSuccessStatus: 200,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-  })
-);
+server.use(cors({ origin: corsOptionsDelegate }));
 
 connectDB();
 
 server.use(helmet());
-
-// CORS authorisation
 
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
