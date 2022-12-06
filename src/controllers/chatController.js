ee5e8d318bf4f6a0b8f939f6ba0ff02edef92893
pablo-zalazar <pablo_zalazar@outlook.com>
2022-12-05 21:38:07 -0300

    commit

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,4 +1,6 @@
+import Chat from "../models/Chat.js";
 import Phrase from "../models/Phrase.js";
+import User from "../models/User.js";
 
 export const getPhrases = async (req, res) => {
   try {
@@ -49,6 +51,59 @@ export const deletePhrase = async (req, res) => {
       return res.status(404).json({ msg: error.message });
     }
     return res.json({ phrase: deletedPhrase.phrase });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getChat = async (req, res) => {
+  const { userId } = req.params;
+  const { userID } = req;
+  try {
+    const friend = await User.findById(userId);
+    if (!friend) {
+      const error = new Error("Friend id not exists");
+      return res.status(403).json({ msg: error.message });
+    }
+    const user = await User.findById(userID).populate("chatHistories");
+    if (user.chatHistories) {
+      const currentChat = user.chatHistories.filter(
+        chat =>
+          (chat.firstUserId.toString() === userID && chat.secondUserId.toString() === userId) ||
+          (chat.firstUserId.toString() === userId && chat.secondUserId.toString() === userID)
+      );
+      if (currentChat.length < 1) {
+        const newChat = new Chat({ firstUserId: userID, secondUserId: userId });
+        user.chatHistories.push(newChat._id);
+        friend.chatHistories.push(newChat._id);
+        await newChat.save();
+        await user.save();
+        await friend.save();
+        console.log("1");
+        return res.json(newChat.messages);
+      }
+      console.log("2");
+      return res.json(currentChat[0]);
+    }
+    return res.send("error");
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export const setChat = async (req, res) => {
+  const { chatId } = req.params;
+  const { message } = req.body;
+  const { userID } = req;
+  try {
+    const currentChat = await Chat.findById(chatId);
+    if (!currentChat) {
+      const error = new Error("Chat not exists");
+      return res.status(403).json({ msg: error.message });
+    }
+    currentChat.messages.push({ id: userID, message });
+    await currentChat.save();
+    return res.json(currentChat);
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
---
+++ src/models/Chat.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const Message = mongoose.Schema(
+  {
+    id: {
+      type: String
+    },
+    message: {
+      type: String
+    }
+  },
+  { _id: false }
+);
+
+const chatSchema = mongoose.Schema(
+  {
+    firstUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true
+    },
+    secondUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true
+    },
+    room: {
+      type: String,
+      require: true,
+      default: ""
+    },
+    messages: {
+      type: [Message],
+      default: []
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+export default Chat;
