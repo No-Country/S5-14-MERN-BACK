1beac887c38bb239bbd63c4bb2f9b1161f6a190f
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-07 21:04:20 +0100

    push tardio

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import server from "./src/server/index.js";
-import serverSocket from "./src/server/socket.js";
+//import serverSocket from "./src/server/socket.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-serverSocket.listen(PORT, () => console.log(`Server running on http:localhost:${PORT}/api`));
+server.listen(PORT, () => console.log(`Server running on http:localhost:${PORT}/api`));
 
-serverSocket.on("error", error => {
+server.on("error", error => {
   LogError(`Server error: ${error}`);
 });
