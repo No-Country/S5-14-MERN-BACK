3c9c7a9d2cf55880fa738266495cd214d4de9cbb
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-08 16:12:51 +0100

    images

--- src/server/index.js
+++ src/server/index.js
@@ -84,7 +84,7 @@ server.route("/api/message").post((req, res) => {
 server.use("/images", express.static(path.join(__dirname, "images")));
 server.use(express.static(path.join(__dirname, "dist")));
 server.use(express.static(path.join(__dirname, "dist/assets")));
-server.use("/assets/*", (req, res) => {
+server.use("/assets", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/assets"));
 });
 
